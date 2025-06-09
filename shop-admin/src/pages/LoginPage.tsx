1601332a43146b71206d9b78a9ae13d7e9c60961
LucyKorotkova <lusi5000@icloud.com>
2025-06-09 12:11:06 +0300

    Update LoginPage.tsx

--- shop-admin/src/pages/LoginPage.tsx
+++ shop-admin/src/pages/LoginPage.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 export default function LoginPage() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    alert(`Логин: ${login}, Пароль: ${password}`);
+  }
+
   return (
     <div>
       <h2>Вход в админку</h2>
-      {/* РЕАЛИЗОВАТЬ форму логина  */}
-      <p>Заглушка для логина</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Логин"
+          value={login}
+          onChange={e => setLogin(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Пароль"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button type="submit">Войти</button>
+      </form>
     </div>
   );
 }
